43d3576f733dd8cc208573f7384ae3a681063372
Administrador <mateo_posada82231@elpoli.edu.co>
2025-09-03 13:21:26 -0500

    inicio

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+    type: 'content',
+        schema: z.object({
+            title: z.string(),
+            date: z.date(),
+            description: z.string(),
+            image: z.string(),
+
+            //relación
+            author: z.string(),
+            //relación
+            tags: z.array(z.string()),
+    })
+})
+
+export const collections = {
+    blog: blogCollection,
+}
---
+++ src/utils/formatter.ts
@@ -0,0 +1,9 @@
+export class Formatter {
+    static formatDate(date: Date): string {
+        return new Intl.DateTimeFormat('es-ES', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        }).format(date);
+    }
+}
